2487fe079669dd4c3abf1d1a7d2e4bba0f5e98b2
ramiya shree <ramiyaseshaiah@gmail.com>
2020-04-17 22:06:46 +0530

    Quality of the code improved

--- app.js
+++ app.js
@@ -3,10 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const _ = require("lodash");
-
 const mongoose = require("mongoose");
-
-
 
 const app = express();
 
@@ -15,12 +12,9 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-//mongoose.connect("mongodb://localhost:27017/bookmarkintern", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/bookmarkintern", {useNewUrlParser: true});
 
-mongoose.connect("mongodb+srv://admin-ramiya:Test123@cluster0-xdeqa.mongodb.net/bookmarkintern", {useNewUrlParser: true});
-
-
-
+//mongoose.connect("mongodb+srv://admin-ramiya:Test123@cluster0-xdeqa.mongodb.net/bookmarkintern", {useNewUrlParser: true});
 
 const itemsSchema = {
   name: String
@@ -30,19 +24,19 @@ const Item = mongoose.model("Item",itemsSchema);
 
 const item1 = new Item ({
 
- name: "https://www.nytimes.com/2020/04/14/us/politics/trump-total-authority-claim.html"
+  name: "https://www.nytimes.com/2020/04/14/us/politics/trump-total-authority-claim.html"
 
 });
 
 const item2 = new Item ({
 
- name: "https://www.google.com/"
+  name: "https://www.google.com/"
 
 });
 
 const item3 = new Item ({
 
- name: "https://robbishop.house.gov/media/press-releases/house-members-take-cause-liberty"
+  name: "https://robbishop.house.gov/media/press-releases/house-members-take-cause-liberty"
 
 });
 
@@ -55,16 +49,12 @@ const listSchema = {
 
 const List = mongoose.model("List",listSchema);
 
-
-//INITIAL PAGE WITH THE DEFAULT BOOKMARKS
+//INITIAL HOME PAGE WITH THE DEFAULT BOOKMARKS
 app.get("/", function(req, res) {
   List.find({},function(err,lists){
     Item.find({}, function(err,items){
-
       if(items.length == 0){
-
         Item.insertMany(defaultItems,function(err){
-
           if(err){
             console.log(err)
           }
@@ -74,264 +64,161 @@ app.get("/", function(req, res) {
         });
         res.redirect("/");
       }
-
       else{
-        //res.redirect("/");
-
         res.render("list", {listTitle: "Home Page", newListItems: items, lists: lists});
       }
-
     });
- });
-
   });
 
+});
 
-// SPECIFIC TAG BOOKMARKS ARE RETRIVED
+// SPECIFIC TAG WITH THEIR BOOKMARKS PAGE IS RETRIVED
 app.get("/:customListName", function(req, res){
-
   const customListName = req.params.customListName;
- List.find({},function(err,lists){
-  List.findOne({name: customListName} , function(err, foundItemsofList){
-   if(!err){
-     if(!foundItemsofList){
-       const list = new List({
-
-        name: customListName,
-        items: defaultItems
-      });
-
-       list.save();
-    //   res.send(list);
-       res.redirect("/"+ customListName);
-     }
-     else{
-      // res.send(foundItemsofList); // the list and their items
-        //res. write(foundList.items); //just the items of the list
-       res.render("list",{listTitle: foundItemsofList.name , newListItems: foundItemsofList.items,lists:lists});
-    }
-  }
-  else{
-    console.log(err)
-  }
+  List.find({},function(err,lists){
+    List.findOne({name: customListName} , function(err, foundItemsofList){
+      if(!err){
+        if(!foundItemsofList){
+          res.redirect("/"+ customListName);
+        }
+        else{
+          res.render("list",{listTitle: foundItemsofList.name , newListItems: foundItemsofList.items,lists:lists});
+        }
+      }
+      else{
+        console.log(err)
+      }
+    });
+  });
 });
-});
-});
-
-
-
 
 //Creating the tags to add bookmarks to it
 app.post("/createTag",function(req,res){
-var count = 0;
-     var itemName = req.body.newItem;
-          console.log(itemName);
-
-
-
+  var itemName = req.body.newItem;
   var tagName = req.body.newTag;
-  if(tagName.length == 0)
-  {
   const item = new Item({
     name: itemName
   });
-
-      item.save();
-
+  if(tagName.length == 0) //WHEN TO TAGS ARE SEPCIFIED
+  {
+    Item.findOne({name:itemName}, function(err,lists){
+      if(!err){
+        if(!lists){
+          item.save();
+        }
+      }
       res.redirect("/");
+    });
+  }
+  else{ //WHEN MULTIPLE TAGS ARE SPECIFIED
+    var string = tagName.split(",");
+    string.forEach(elements => {
+      List.findOne({name: elements} , function(err, foundList){
+        if(!err){
+          if(!foundList){ //WHEN THE TAG IS NOT FOUND IT IS CREATED AND THEN THE BOOKMARK IS ADDED TO THE TAG
+            const list = new List({
+              name: elements,
+            });
+            list.items.push(item);
+            list.save();
+          }
+          else{
+            List.find({"items.name":itemName}, function(err,lists){ //IF THE TAG IS ALREADY EXISTING WE CHECK WHETHER THS BOOKMARK IS ALREADY PRESENT TO AVOID DUPLICATE COPY
+              if(lists.length == 0){
+                foundList.items.push(item);
+                foundList.save();
+              }
+            });
+          }
+        }
+        else{
+          console.log(err)
+        }
+      });
+    });
+    res.redirect("/");
+  }
+});
 
-      console.log(tagName);
+//RETRIVING ALL BOOKMARKS FROM THE DEFAULT AND FROM TAGS
+app.post("/getAllBookmarks",function(req,res){
+  List.find({},function(err,lists){
+    Item.find({}, function(err,items){
+      var itemsName = [];
+      if(err)
+      {
+        console.log(err);
+      }
+      else{
+        items.forEach(function(item){ //BOOKMARKS WITHOUT TAGS
+          itemsName.push(item.name);
+        });
+        lists.forEach(function(list){  //BOOKMARKS WITH TAGS
+          list.items.forEach(function(name){
+            itemsName.push(name.name);
+          });
+        });
+        res.render("about", {listTitle:"BookMarks",listdata:itemsName});
+      }
+    });
+  });
+});
+
+//RETRIVING ALL TAGS
+app.post("/getAllTags",function(req,res){
+  List.find({},function(err,lists){
+    var tagsName = ["Home"];
+    if(err)
+    {
+      console.log(err);
     }
     else{
-  var string = tagName.split(",");
-      console.log(string);
-  string.forEach(elements => {
-   List.findOne({name: elements} , function(err, foundList){
-   if(!err){
-     if(!foundList){
-       console.log("not found creating");
-       const list = new List({
-
-        name: elements,
-
-      });
-
-       list.save();
-
-    console.log(elements);
-
-              const item = new Item({
-                    name: itemName
-                });
-
-
-              list.items.push(item);
-                  list.save();
-              console.log("found it");
-              // res.redirect("/");
-
-
-
-   }
-     else{
-
-             List.find({"items.name":itemName}, function(err,lists){
-               if(lists.length == 0){
-
-                     const item = new Item({
-                           name: itemName
-                       });
-
-
-                       foundList.items.push(item);
-
-                           foundList.save();
-                       foundList.save();
-                       console.log("found it");
-
-
-               }
-
-
-            });
-
-    }
-    // res.redirect("/");
-  }
-  else{
-    console.log(err)
-  }
-});
-});
-
-res.redirect("/");
-
-
-
-
-}
-});
-
-
-
-//Retriving all bookmarks
-app.post("/getAllBookmarks",function(req,res){
-List.find({},function(err,lists){
-      Item.find({}, function(err,items){
-  var itemsName = [];
-  if(err)
-  {
-    console.log(err);
-  }
-  else{
-    items.forEach(function(item){
-      itemsName.push(item.name);
-    });
-
-    lists.forEach(function(list){
-      list.items.forEach(function(name){
-        //console.log(name.name);
-        itemsName.push(name.name);
-
-      });
-  });
-
-  console.log(itemsName);
-    res.render("about", {listTitle:"BookMarks",listdata:itemsName});
-
-}
-});
-});
-});
-
-
-//Retriving all tags
-app.post("/getAllTags",function(req,res){
-List.find({},function(err,lists){
-  var tagsName = ["Home"];
-
-  if(err)
-  {
-
-    console.log(err);
-  }
-  else{
-    console.log(lists);
-    lists.forEach(function(list){
-        //console.log(name.name);
+      lists.forEach(function(list){
         tagsName.push(list.name);
-
       });
-
-  //console.log(itemsName);
-    res.render("about", {listTitle:"TAGS",listdata:tagsName});
-  //  res.render("about", {list: "lists"});
-  }
-});
+      res.render("about", {listTitle:"TAGS",listdata:tagsName});
+    }
+  });
 });
 
-
-//deleting specific bookmark from the folder or tags
+//DELETING A BOOKMARK FROM A SPECIFIC TAG
 app.post("/delete",function(req, res){
-
   const checkedItemId = req.body.checkbox;
   const listName = req.body.listName;
-console.log(listName);
-  if(listName === "Home Page"){
+  if(listName === "Home Page"){ //DELETING DEFAULT BOOKMARK THOSE WITHOUT TAGS
     Item.findByIdAndRemove(checkedItemId, function(err){
       if(err){
-       console.log(err);
-       console.log("error");
-     }
-     else{
-
-      console.log("Successfully deleted");
-      res.redirect("/");
-      //res.send("deleted Successfully");
-    }
-  });
-  }
-
-  else {
-    List.findOneAndUpdate(
-     {name: listName},
-     {$pull: {items: {_id:checkedItemId}}},
-     function(err, foundList){
-      if(!err){
-        console.log("deleted");
-
-        //res.send("deleted Successfully");
-        res.redirect("/"+listName);
+        console.log(err);
       }
-    }
-
+      else{
+        res.redirect("/");
+      }
+    });
+  }
+  else {
+    List.findOneAndUpdate( //DELETING BOOKMARKS FROM A TAG
+      {name: listName},
+      {$pull: {items: {_id:checkedItemId}}},
+      function(err, foundList){
+        if(!err){
+          res.redirect("/"+listName);
+        }
+      }
     )
   }
-
 });
 
-app.post("/deleteTag",function(req, res){
-
+app.post("/deleteTag",function(req, res){ //REMOVING A TAG
   const checkedItemId = req.body.checkbox;
   const listName = req.body.listName;
-console.log(listName);
-
-
-
-    List.deleteOne(
-     {_id: checkedItemId},
-     function(err){
+  List.deleteOne(
+    {_id: checkedItemId},
+    function(err){
       if(!err){
-        console.log("deleted");
-
-        //res.send("deleted Successfully");
         res.redirect("/");
       }
     }
-
-    )
-
-
+  )
 });
 
 let port = process.env.PORT;
